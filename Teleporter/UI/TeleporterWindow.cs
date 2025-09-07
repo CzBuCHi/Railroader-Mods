bf5c3c7edde0461e6f01729ebc47218c0f5e6ae3
Marek Buchar <czbuchi@gmail.com>
2025-09-07 19:27:54 +0200

    Teleporter: ui bug fix, ability to rearrange groups

--- Teleporter/UI/TeleporterWindow.cs
+++ Teleporter/UI/TeleporterWindow.cs
@@ -18,20 +18,17 @@ public sealed class TeleporterWindow : ProgrammaticWindowBase
 {
     public override Window.Sizing Sizing => Window.Sizing.Resizable(new(400, 250));
 
-    private string          _NewLocationGroup = "Unnamed";
-    private string          _NewLocationName  = "";
-    private UIState<string> _SelectedGroup    = new("");
+    private string              _NewLocationGroup = "Unnamed";
+    private string              _NewLocationName  = "";
+    private UIState<string>     _SelectedGroup    = new("");
 
-    private HashSet<string> _Groups;
+    private List<LocationGroup> Groups => TeleporterPlugin.Settings.Groups;
 
     private static TeleporterWindow Shared => WindowManager.Shared!.GetWindow<TeleporterWindow>()!;
 
     public override void Awake() {
         base.Awake();
         Window.Title = "Teleporter";
-
-        _Groups = new HashSet<string>(TeleporterPlugin.Settings.Groups.Select(o => o.Name));
-        _SelectedGroup.Value = _Groups.FirstOrDefault() ?? "";
     }
 
     protected override void WindowOnOnShownDidChange(bool isShown) {
@@ -52,18 +49,21 @@ public sealed class TeleporterWindow : ProgrammaticWindowBase
 
     protected override void Build(UIPanelBuilder builder) {
         builder.RebuildOnEvent<SettingsChangedEvent>();
-
+      
         builder.ButtonStrip(strip => {
             strip.AddButton("Save current", Add)
                  .Tooltip("Save current position", "Save current location under new name");            
         });
-        builder.AddField("New location group", builder.AddInputField(_NewLocationGroup, UpdateNewLocationGroup, "Group name for new location"));
+        builder.AddField("Group name", builder.AddInputField(_NewLocationGroup, UpdateNewLocationGroup, "Group name for new location"));
         builder.AddField("New location name", builder.AddInputField(_NewLocationName, UpdateNewLocationName, "Unique name for new location"));
 
-        var groups = _Groups
+        var groups = Groups
+                     .Select(o => o.Name)
                      .Select(o => new UIPanelBuilder.ListItem<string>(o, o, "", o))
                      .ToList();
-     
+
+        _SelectedGroup.Value ??= groups.Select(o => o.Value).FirstOrDefault() ?? "";
+
         builder.AddListDetail(groups, _SelectedGroup, BuildGroup);
 
         return;
@@ -83,28 +83,41 @@ public sealed class TeleporterWindow : ProgrammaticWindowBase
         builder.RebuildOnEvent<SettingsChangedEvent>();
 
         if (string.IsNullOrEmpty(groupName)) {
-            var message = TeleporterPlugin.Settings.Groups.Count > 0
+            var message = Groups.Count > 0
                 ? "Select group to view locations ..."
                 : "Saved locations will be shown here ...";
             builder.AddLabel(message);
             return;
         }
 
-        var group = TeleporterPlugin.Settings.Groups.FirstOrDefault(o => o.Name == groupName);
-        if (group == null) {
-            builder.AddLabel("Something went wrong ... (cannot find group named '" + groupName + "'");
+        var index = Groups.FindIndex(o => o.Name == groupName);
+        if (index == -1) {
+            builder.AddLabel($"Something went wrong ... (cannot find group named '{groupName}'");
             return;
         }
+
+        builder.AddField("Group",
+            builder.ButtonStrip(strip => {
+                strip.AddButton("Move Up", SwapGroups(index, index - 1))
+                     .Tooltip("Move group up", "Move group up in list of groups")!
+                     .Disable(index == 0);
+                strip.AddButton("Move Down", SwapGroups(index, index + 1))
+                     .Tooltip("Move group down", "Move group down in list of groups")!
+                     .Disable(index == Groups.Count - 1);
+            })
+        );
+
+        var group = Groups[index]!;
 
         builder.VScrollView(scroll => {
             foreach (var pair in group.Locations.OrderBy(o => o.Key)) {
                 scroll.AddField(pair.Key!,
                     scroll.ButtonStrip(strip => {
-                        strip.AddButton(TextSprites.Destination, TeleportTo(pair.Value!))!.Tooltip("Teleport", "Switch to strategy camera at given location");
+                        strip.AddButton(TextSprites.Destination, TeleportTo(pair.Value!)).Tooltip("Teleport", "Switch to strategy camera at given location");
                         strip.Spacer();
-                        strip.AddButton("Replace", Replace(pair.Key!))!.Tooltip("Replace", "Save current location under this name");
-                        strip.AddButton("Remove", Remove(pair.Key!))!.Tooltip("Remove location", "Remove location from saved locations");
-                    })!
+                        strip.AddButton("Replace", Replace(pair.Key!)).Tooltip("Replace", "Save current location under this name");
+                        strip.AddButton("Remove", Remove(pair.Key!)).Tooltip("Remove location", "Remove location from saved locations");
+                    })
                 );
             }
         });
@@ -115,9 +128,9 @@ public sealed class TeleporterWindow : ProgrammaticWindowBase
             return () => {
                 var point    = location.Position.ToVector3();
                 var rotation = location.Rotation.ToQuaternion();
-                CameraSelector.shared!.SelectCamera(CameraSelector.CameraIdentifier.Strategy);
+                CameraSelector.shared.SelectCamera(CameraSelector.CameraIdentifier.Strategy);
 
-                var camera = CameraSelector.shared!.strategyCamera!;
+                var camera = CameraSelector.shared.strategyCamera;
                 camera.SetAngleX(rotation.eulerAngles.x);
                 camera.SetDistance(location.Distance);
                 camera.JumpTo(point, rotation);
@@ -130,22 +143,29 @@ public sealed class TeleporterWindow : ProgrammaticWindowBase
             return () => {
                 group.Locations.Remove(key);
                 if (group.Locations.Count == 0) {
-                    TeleporterPlugin.Settings.Groups.Remove(group);
+                    Groups.Remove(group);
                 }
 
+                TeleporterPlugin.SaveSettings();
+            };
+        }
+        
+        Action SwapGroups(int first, int second) {
+            return () => {
+                (Groups[first], Groups[second]) = (Groups[second], Groups[first]);
                 TeleporterPlugin.SaveSettings();
             };
         }
     }
     
     private void Add() {
-        var group = TeleporterPlugin.Settings.Groups.FirstOrDefault(o => o.Name == _NewLocationGroup);
+        var group = Groups.FirstOrDefault(o => o.Name == _NewLocationGroup);
         if (group == null) {
             group = new LocationGroup(_NewLocationGroup, new Dictionary<string, TeleportLocation>());
-            TeleporterPlugin.Settings.Groups.Add(group);
+            Groups.Add(group);
         } else {
             if (group.Locations.ContainsKey(_NewLocationName)) {
-                Toast.Present("Location with name " + _NewLocationName + " already exists in group + " + _NewLocationGroup + ".");
+                Toast.Present($"Location with name {_NewLocationName} already exists in group '{_NewLocationGroup}'.");
                 return;
             }
         }
@@ -154,10 +174,10 @@ public sealed class TeleporterWindow : ProgrammaticWindowBase
     }
 
     private void Save(string groupName, string locationName) {
-        var group = TeleporterPlugin.Settings.Groups.FirstOrDefault(o => o.Name == _NewLocationGroup);
+        var group = Groups.FirstOrDefault(o => o.Name == _NewLocationGroup);
         if (group == null) {
             group = new LocationGroup(_NewLocationGroup, new Dictionary<string, TeleportLocation>());
-            TeleporterPlugin.Settings.Groups.Add(group);
+            Groups.Add(group);
         } 
 
         Save(group, locationName);
